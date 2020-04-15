7aaab55f5ebbeae76db858205260c3535cb0c3c7
Cristhian <csotelo88@hotmail.com>
2020-04-14 18:24:58 -0600

    got swipe views working

--- app/src/main/java/com/example/cuponastring/ContactA.java
+++ app/src/main/java/com/example/cuponastring/ContactA.java
@@ -3,10 +3,14 @@ package com.example.cuponastring;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,7 +36,7 @@ public class ContactA extends Fragment {
 
         contactName.setText(cName);
 
-        int id = getResources().getIdentifier(cImage, "drawable", "your.package.name");
+        int id = getResources().getIdentifier(cImage, "drawable", "com.example.cuponastring");
         contactImage.setImageResource(id);
 
         return view;
@@ -45,12 +49,30 @@ public class ContactA extends Fragment {
     }
 
     public void turnCup(boolean up) {
+        float normalSize = (float) 1.0;
+        float largeSize = (float) 1.5;
+        Log.d("tag", "got to turnCup()");
+        /*
         if (up) {
             //enlarge contact image
+            redCup.setVisibility(View.INVISIBLE);
+            Animation scale = new ScaleAnimation(normalSize, largeSize, normalSize, largeSize);
+            scale.setDuration(750);
+            AnimationSet animSet = new AnimationSet(true);
+            animSet.setFillEnabled(true);
+            animSet.addAnimation(scale);
+            contactName.startAnimation(animSet);
         }
         else {
             //bring contact image to normal size
-        }
+            redCup.setVisibility(View.VISIBLE);
+            Animation scale = new ScaleAnimation(largeSize, normalSize, largeSize, normalSize);
+            scale.setDuration(750);
+            AnimationSet animSet = new AnimationSet(true);
+            animSet.setFillEnabled(true);
+            animSet.addAnimation(scale);
+            contactName.startAnimation(animSet);
+        }*/
     }
 
 }
--- app/src/main/java/com/example/cuponastring/MainActivity.java
+++ app/src/main/java/com/example/cuponastring/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-public abstract class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     ViewPager pager;
     ViewPagerAdapter viewPagerAdapter;
@@ -34,8 +34,8 @@ public abstract class MainActivity extends AppCompatActivity implements SensorEv
         pager = findViewById(R.id.pager);
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         ContactA contact1 = new ContactA("Contact 1", "forsen_cd");
-        ContactA contact2 = new ContactA("Contact 2", "cute-duck");
-        ContactA contact3 = new ContactA("Contact 3", "cute-piggy");
+        ContactA contact2 = new ContactA("Contact 2", "cute_duck");
+        ContactA contact3 = new ContactA("Contact 3", "cute_piggy");
 
         viewPagerAdapter.addContact(contact1);
         viewPagerAdapter.addContact(contact2);
@@ -44,6 +44,7 @@ public abstract class MainActivity extends AppCompatActivity implements SensorEv
 
         // initialize your android device sensor capabilities
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
 
         //set the screen to fullscreen
         View view = getWindow().getDecorView();
@@ -85,120 +86,22 @@ public abstract class MainActivity extends AppCompatActivity implements SensorEv
 
         // get the angle around the z-axis rotated
         float degree = Math.round(event.values[1]);
+        ContactA currentView = (ContactA) viewPagerAdapter.getItem(pager.getCurrentItem());
 
         if(currentDegree < (talkDegree + 10) && currentDegree > (talkDegree - 10))
         {
             //call method in contact page to enlarge contact's image
-
+            currentView.turnCup(true);
         }
         else {
             //call method to show red cup again
+            currentView.turnCup(false);
         }
-    }
-
-
-    /*
-    // define the display assembly compass picture
-    private ImageView image;
-
-    // record the compass picture angle turned
-    private float currentDegree = 0f;
-    private float talkDegree = 0f;
-
-    // device sensor manager
-    private SensorManager mSensorManager;
-
-    TextView tvHeading;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //
-        image = (ImageView) findViewById(R.id.red_cup_down);
-
-        // TextView that will tell the user what degree is he heading
-        tvHeading = (TextView) findViewById(R.id.tvHeading);
-
-        // initialize your android device sensor capabilities
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-
-        //set the screen to fullscreen
-        View view = getWindow().getDecorView();
-        view.setSystemUiVisibility
-            (
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                    // Set the content to appear under the system bars so that the
-                    // content doesn't resize when the system bars hide and show.
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    // Hide the nav bar and status bar
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-            );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // to stop the listener and save battery
-        mSensorManager.unregisterListener(this);
-    }
-
-    //Where it determines the position of the device by degrees
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        // get the angle around the z-axis rotated
-        float degree = Math.round(event.values[1]);
-
-        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
-
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -degree,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-
-        // how long the animation will take place
-        ra.setDuration(210);
-
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-
-        // Start the animation
-        //image.startAnimation(ra);
-        currentDegree = -degree;
-
-        if(currentDegree < (talkDegree + 10) && currentDegree > (talkDegree - 10))
-        {
-            image.setImageResource(R.drawable.forsen_cd);
-        }
-        else
-        {
-            image.setImageResource(R.drawable.red_cup_down);
-        }
-
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // not in use
+        //not in use
     }
-    */
+
 }
